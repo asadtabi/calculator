917c1b47e21b708f4ad8ef008a31a7a565e6dd21
asad tabi <bcsf18a5172pucit.edu.pk>
2021-10-20 00:55:33 +0500

    buttons added

--- app/src/main/java/com/rabia/arif/calculator/MainActivity.java
+++ app/src/main/java/com/rabia/arif/calculator/MainActivity.java
@@ -10,6 +10,14 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button0, button1, button2, button3, button4, button5, button6,
+            button7, button8, button9, buttonAdd, buttonSub, buttonDivision,
+            buttonMul, button10, buttonC, buttonEqual;
+    EditText crunchifyEditText;
+
+    float mValueOne, mValueTwo;
+
+    boolean crunchifyAddition, mSubtract, crunchifyMultiplication, crunchifyDivision;
     
 
     @Override
